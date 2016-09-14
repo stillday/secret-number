a7ab6d814887f0cb12d75818801c6d51b669e377
Jan Drenkhahn <jan-dennisd@outlook.at>
2016-09-14 10:06:23 +0200

    eine schleife eingebaut

--- secret.py
+++ secret.py
@@ -3,18 +3,21 @@ secret = 83
 
 print "Errate die Geheim Zahl, gib deinen Tip ab"
 print "Die Zahl befindet sich zwischen 0 und 100"
-try:
-    guess = int(raw_input("Gib deinen Tip ab:  "))
+print "Du hast 5 Versuche"
 
-    if guess == secret:
-        print "Dein Tip war richtig"
+for x in range(5):
+    try:
+        guess = int(raw_input("Gib deinen Tip ab:  "))
 
-    elif guess < secret:
-        print "Deine Tip war zu klein. Versuch es nochmal"
+        if guess == secret:
+            print "Dein Tip war richtig"
+            break
 
-    elif guess > secret:
-        print "Dein Tip war zu gross. Versuche es nochmal"
+        elif guess < secret:
+            print "Deine Tip war zu klein. Versuch es nochmal"
 
-except ValueError:
-    print "Gib bitte eine Zahl ein."
-    exit ()
+        elif guess > secret:
+            print "Dein Tip war zu gross. Versuche es nochmal"
+
+    except ValueError:
+        print "Gib bitte eine Zahl ein."
