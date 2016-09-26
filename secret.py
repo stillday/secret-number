a0bce5704f8cf42f73e35464278e1d5128b78e2a
Jan Drenkhahn <jan-dennisd@outlook.at>
2016-09-26 15:52:40 +0200

    Homework 10.2 Update

--- secret.py
+++ secret.py
@@ -1,23 +1,33 @@
+import random
+def main():
+    secret = random.randint(0, 100)
 
-secret = 83
+    print "Errate die Geheim Zahl, gib deinen Tip ab"
+    print "Die Zahl befindet sich zwischen 0 und 100"
+    print "Du hast 5 Versuche"
 
-print "Errate die Geheim Zahl, gib deinen Tip ab"
-print "Die Zahl befindet sich zwischen 0 und 100"
-print "Du hast 5 Versuche"
+    list = []
 
-for x in range(5):
-    try:
-        guess = int(raw_input("Gib deinen Tip ab:  "))
+    for x in range(10):
+        try:
+            guess = int(raw_input("Gib deinen Tip ab:  "))
+            list.append(guess)
 
-        if guess == secret:
-            print "Dein Tip war richtig"
-            break
+            if guess == secret:
+                print "Dein Tip war richtig"
+                break
 
-        elif guess < secret:
-            print "Deine Tip war zu klein. Versuch es nochmal"
+            elif guess < secret:
+                print "Deine Tip war zu klein. Versuch es nochmal"
 
-        elif guess > secret:
-            print "Dein Tip war zu gross. Versuche es nochmal"
+            elif guess > secret:
+                print "Dein Tip war zu gross. Versuche es nochmal"
 
-    except ValueError:
-        print "Gib bitte eine Zahl ein."
+        except ValueError:
+            print "Gib bitte eine Zahl ein."
+
+    print "Das waren alle deine Eingaben: %s" % list
+    print "Du hast %s Versuche gebraut" % len(list)
+    print "Ende"
+if __name__ == '__main__':
+    main()
