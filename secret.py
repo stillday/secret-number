0b48dfdbe766c0cbb3b62663fa29ae2d5382d38d
Jan Drenkhahn <jan-dennisd@outlook.at>
2016-10-03 10:22:32 +0200

    now with gui

--- secret.py
+++ secret.py
@@ -1,33 +1,47 @@
 import random
-def main():
-    secret = random.randint(0, 100)
+import Tkinter
+import tkMessageBox
 
-    print "Errate die Geheim Zahl, gib deinen Tip ab"
-    print "Die Zahl befindet sich zwischen 0 und 100"
-    print "Du hast 5 Versuche"
 
-    list = []
+secret = random.randint(0, 100)
+list = []
 
-    for x in range(10):
-        try:
-            guess = int(raw_input("Gib deinen Tip ab:  "))
-            list.append(guess)
+window = Tkinter.Tk()
 
-            if guess == secret:
-                print "Dein Tip war richtig"
-                break
+def quit():
+    global window
+    window.quit()
 
-            elif guess < secret:
-                print "Deine Tip war zu klein. Versuch es nochmal"
+greeting = Tkinter.Label(window, text="Errate die Geheim Zahl, gib deinen Tip ab.")
+greeting.pack()
+greeting = Tkinter.Label(window, text="Die Zahl befindet sich zwischen 0 und 100.")
+greeting.pack()
+greeting = Tkinter.Label(window, text="Du hast 10 Versuche.")
+greeting.pack()
 
-            elif guess > secret:
-                print "Dein Tip war zu gross. Versuche es nochmal"
 
-        except ValueError:
-            print "Gib bitte eine Zahl ein."
 
-    print "Das waren alle deine Eingaben: %s" % list
-    print "Du hast %s Versuche gebraut" % len(list)
-    print "Ende"
-if __name__ == '__main__':
-    main()
+guess = Tkinter.Entry(window)
+guess.pack()
+
+for x in range(10):
+
+    def guess_check():
+
+        if int(guess.get()) == secret:
+            result_text = "Dein Tip war richtig. Folgende Zahlen hast du eingeben %s." % list
+            window.quit()
+
+        elif int(guess.get()) < secret:
+            result_text = "Deine Tip war zu klein. Versuch es nochmal."
+
+        else:
+            result_text = "Dein Tip war zu gross. Versuche es nochmal"
+
+        tkMessageBox.showinfo("Result", result_text)
+
+# submit button
+submit = Tkinter.Button(window, text="Submit", command= guess_check)  # check_guess, not check_guess()
+submit.pack()
+
+window.mainloop()
